bc76f919dbef8f099aa343f9d2c9f16c4dc23f70
Fabrizio Andre <fabriziodidthis@outlook.com>
2024-06-29 21:58:13 -0300

    Retrieve user data from DB & refactor

---
+++ src/commands/showUserDataFromDatabase.ts
@@ -0,0 +1,39 @@
+import { githubUserFound, IGithubUser } from '../constants/types.js'
+import { retrieveUserDataFromDatabase } from '../helpers/retrieveUserDataFromDatabase.js'
+import { showUserImage } from '../helpers/showUserImage.js'
+
+/**
+ * @description Show user information saved in the database
+ * @param userData
+ * @returns A table with the user information
+ */
+
+const showUserDataFromDatabase = async (
+  username: string,
+): Promise<IGithubUser | null> => {
+  console.log(`
+  -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+        Retrieving user data from database for - \x1b[31m${username}\x1b[0m -
+
+  -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+  `)
+
+  try {
+    const userData = (await retrieveUserDataFromDatabase(
+      username,
+    )) as githubUserFound
+
+    await showUserImage(userData)
+
+    console.table(userData)
+  } catch (error) {
+    console.error(
+      'Something went wrong while fetching the user, try again later.',
+      error,
+    )
+  }
+  return null
+}
+
+export { showUserDataFromDatabase }
---
+++ src/helpers/retrieveUserDataFromDatabase.ts
@@ -0,0 +1,33 @@
+import { db } from '../database/config/pgpromise.js'
+import { githubUserFound, IGithubUser } from '../constants/types.js'
+
+/**
+ * @description Fetch user data from Database
+ * @param username string
+ * @returns A table with the user information
+ */
+const retrieveUserDataFromDatabase = async (
+  login: string,
+): Promise<IGithubUser | null> => {
+  try {
+    const retrieveUserData = await db.oneOrNone(
+      'SELECT * FROM github_users WHERE login = $1',
+      [login] as string[],
+    )
+    if (!retrieveUserData) {
+      console.error(`The username ${login} could not be found in the database.`)
+    }
+
+    const retrieveUserDataToJSON: githubUserFound =
+      (await retrieveUserData) as githubUserFound
+
+    return retrieveUserDataToJSON
+  } catch (error) {
+    console.error(
+      'Something went wrong while retrieving the user, try again later.',
+      error,
+    )
+  }
+  return null
+}
+export { retrieveUserDataFromDatabase }
